502291ed9dc6544385841074ec5db42a97c7144d
Wenderson Dhomini <wendersondhomini125@gmail.com>
2026-07-20 00:03:09 -0300

    feat: adicionar configurações do kafka

---
+++ servico-pagamentos/src/main/java/br/edu/ifpb/servico_pagamentos/config/KafkaConsumerConfig.java
@@ -0,0 +1,18 @@
+package br.edu.ifpb.servico_pagamentos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public DefaultErrorHandler kafkaErrorHandler(KafkaOperations<Object, Object> kafkaOperations) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaOperations);
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(2000L, 3));
+    }
+}
--- servico-pagamentos/src/main/java/br/edu/ifpb/servico_pagamentos/controller/WebhookController.java
+++ servico-pagamentos/src/main/java/br/edu/ifpb/servico_pagamentos/controller/WebhookController.java
@@ -1,12 +1,15 @@
 package br.edu.ifpb.servico_pagamentos.controller;
 
 import br.edu.ifpb.servico_pagamentos.controller.request.AsaasWebhookRequest;
-import br.edu.ifpb.servico_pagamentos.service.PagamentoService;
+import br.edu.ifpb.servico_pagamentos.kafka.KafkaTopics;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,20 +23,26 @@ import java.util.Map;
 @AllArgsConstructor
 public class WebhookController {
 
-    private final PagamentoService pagamentoService;
+    private static final Logger log = LoggerFactory.getLogger(WebhookController.class);
+
+    private final KafkaTemplate<Object, Object> kafkaTemplate;
 
     @Operation(
             summary = "Recebe notificações de eventos do Asaas",
             description = "Configurado no painel do Asaas. Não é chamado pelo front-end/Postman " +
                     "em uso normal - serve para o gateway avisar sobre confirmação, recusa ou " +
-                    "estorno de um pagamento de forma assíncrona."
+                    "estorno de um pagamento de forma assíncrona. O evento é publicado no Kafka " +
+                    "e processado de forma assíncrona por um consumer."
     )
-    @ApiResponse(responseCode = "200", description = "Evento recebido e processado")
+    @ApiResponse(responseCode = "200", description = "Evento recebido e publicado para processamento")
     @PostMapping(consumes = "application/json")
     public ResponseEntity<Map<String, Boolean>> handleWebhook(
             @RequestBody AsaasWebhookRequest request) {
 
-        pagamentoService.processarWebhook(request);
+        log.info("Webhook recebido do Asaas: event={}, paymentId={}",
+                request.event(), request.payment().id());
+
+        kafkaTemplate.send(KafkaTopics.WEBHOOK_RECEBIDO, request.payment().id(), request);
 
         return ResponseEntity.ok(Map.of("received", true));
     }
